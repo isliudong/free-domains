a69ccbad8e88ee203fa9ae45f40702e30069e45d
刘冬 <2351036454@qq.com>
2023-09-20 10:28:22 +0800

    add : ifalls.is-an.app.js

---
+++ domains/ifalls.is-an.app.js
@@ -0,0 +1,15 @@
+addSubDomain({
+  description: 'A website for idea  plugin',
+  domain: 'is-an.app',
+  subdomain: 'ifalls',
+  owner: {
+    email: '2351036454@qq.com',
+  },
+  record: {
+    A: [
+      '141.147.189.7',
+    ],
+  },
+  proxy: false
+});
+
